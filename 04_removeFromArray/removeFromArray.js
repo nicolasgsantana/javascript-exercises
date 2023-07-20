d32d444431a79e511aafc1d34a619438a30e0c96
nicolasgsantana <nicolas07san@gmail.com>
2023-07-20 20:39:58 -0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    const newArray = []
+    for(const aItem of array) {
+        if(items.includes(aItem)) {
+            continue;
+        }
+        newArray.push(aItem);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
