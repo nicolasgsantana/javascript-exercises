2ec1c46de7e6aae6ae7b7d9e842f46964897a2c7
nicolasgsantana <nicolas07san@gmail.com>
2023-07-20 21:08:30 -0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    if(
+        firstNum < 0 || 
+        secondNum < 0 || 
+        (typeof firstNum != 'number') || 
+        (typeof secondNum != 'number')
+    ) {
+        return "ERROR";
+    }
+
+    let fromNum;
+    let toNum;
+
+    if (firstNum > secondNum) {
+        fromNum = secondNum;
+        toNum = firstNum;
+    }
+    else {
+        fromNum = firstNum;
+        toNum = secondNum;
+    }
+
+    let sum = 0;
+
+    for(let i = fromNum; i < toNum + 1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
