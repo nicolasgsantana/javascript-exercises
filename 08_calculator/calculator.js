332bdea9f95d4f634a7675a36bfdeef0c9d0ba78
nicolasgsantana <nicolas07san@gmail.com>
2023-08-10 14:48:00 -0300

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, n) => sum += n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((sum, n) => sum *= n, 1);
 };
 
-const power = function() {
-	
+const power = function (n1, n2) {
+  return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  const numbers = [];
+  for (let i = n; i > 0; i--) {
+    numbers.push(i);
+  }
+  return multiply(numbers);
 };
 
 // Do not edit below this line
