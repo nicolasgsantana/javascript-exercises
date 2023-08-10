3ff2d3f5cb25bb2ca8ac97490b411c69e5ffec1a
nicolasgsantana <nicolas07san@gmail.com>
2023-08-10 16:56:05 -0300

    First attempt

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    return array.reduce((oldestPerson, person) => {
+        if (oldestPerson === 0) {
+            oldestPerson = person;
+        }
+        const currentYear = new Date().getFullYear();
+        let oldestAge =
+            (oldestPerson.yearOfDeath === 'undefined' ? currentYear : oldestPerson.yearOfDeath) - oldestPerson.yearOfBirth;
 
+        let personAge = (person.yearOfDeath === 'undefined' ? currentYear : person.yearOfDeath) - person.yearOfBirth;
+
+        if (personAge > oldestAge) {
+            oldestPerson = person;
+        }
+    }, 0);
 };
 
 // Do not edit below this line
