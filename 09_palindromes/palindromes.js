a4595e0575f07ad95650f9f826c9480337b7749f
nicolasgsantana <nicolas07san@gmail.com>
2023-08-10 15:07:24 -0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const PUNCTUATIONS = [",", ".", "!"];
 
+    string = string.replaceAll(" ", "");
+    PUNCTUATIONS.forEach(punctuation => {
+        string = string.replaceAll(punctuation, "");
+    });
+
+    string = string.toLowerCase();
+
+    const reversedString = string.split("").reverse().join("");
+
+    return string === reversedString;
 };
 
 // Do not edit below this line
