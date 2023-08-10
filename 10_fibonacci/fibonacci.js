e9aff9f55b7242584c1fb30b67b700497cb311e8
nicolasgsantana <nicolas07san@gmail.com>
2023-08-10 15:48:56 -0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
+    n = +n;
+
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    const fibArray = [];
+    for (let i = 0; i <= n; i++) {
+        if (i > 1) {
+            fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+            continue;
+        }
+        fibArray.push(i);
+    }
+
+    return fibArray[n];
 };
 
 // Do not edit below this line
